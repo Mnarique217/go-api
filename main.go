59ae2202096b16f8099264465131694da9918c9b
Manrique <manri.arf15@gmail.com>
2020-08-16 15:29:55 -0600

    test port 8080 hardcoded

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"log"
 	"net"
-	"os"
 
 	pb "github.com/Mnarique217/go-api/booksapp"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	port := "8080" //os.Getenv("PORT")
 	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
